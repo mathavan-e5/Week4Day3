65d04ed246ec2eca8b6bb1c0f5c23e824fd1da3e
Mathavan M <104998416+volthatter@users.noreply.github.com>
2022-05-26 19:27:46 +0530

    Add files via upload

---
+++ COMPUTER.java
@@ -0,0 +1,23 @@
+package Week4Day3;
+
+
+
+public class COMPUTER {
+	public void computerModel() {
+		
+		System.out.println("computerModel");
+		
+	}
+	
+	
+
+	
+		
+	 
+		
+	
+		
+		
+	
+
+}
---
+++ Desktop.java
@@ -0,0 +1,23 @@
+package Week4Day3;
+
+public class Desktop extends COMPUTER{
+	
+	public void Desktop() {
+		System.out.println("Desktop");
+	}
+		
+		public static void main(String[] args) {
+			
+			Desktop call=new Desktop();
+				call.computerModel();
+				call.Desktop();
+			
+		
+		
+		
+		
+		
+		
+	}
+
+}
